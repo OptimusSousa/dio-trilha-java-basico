b1b2e932890a860ca89aa089177a2ecc24279458
sousa <mauriciocareca78@gmail.com>
2024-05-03 22:04:45 -0300

    operdores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,35 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+		System.out.print(numero);
+
+
+        /* 
+        Para declarar uma variável como uma constante, utilizamos a palavra final, mas por convenção, esta variável deverá ser escrita toda em caixa alta.
+        */ 
+        final double VALOR_DE_PI = 3.14;
+
+
+        /** 
+        Exemplos:
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 21070333; Se começar com zero, talvez seja necessário utilizar outro tipo
+        long cpf = 98765432109L; Se começar com zero, talvez seja necessário utilizar outro tipo  
+        float pi = 3.14f;
+        double salario = 1275.33;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        date dataNascimento = new Date ();
+
+        obs: nos casos do tipo long e float, no final deverá acrescentar as letras L e f respectivamente.
+        **/
     }
 }
