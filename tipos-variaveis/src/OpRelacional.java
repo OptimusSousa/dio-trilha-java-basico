ff6f2175c1303b08be0634b207fdaa05cf249dff
sousa <mauriciocareca78@gmail.com>
2024-05-04 12:16:54 -0300

    update

---
+++ tipos-variaveis/src/OpRelacional.java
@@ -0,0 +1,5 @@
+public class OpRelacional {
+    public static void main(String[] args) {
+        
+    }
+}
--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,26 +1,23 @@
 public class Operadores {
     public static void main(String[] args) {
-        String concatenacao = "?"; 
-
-        concatenacao = 1+1+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+1+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-
-        System.out.println(concatenacao);
-
+        
+        
+      
     }
 }
+
+
+/*
+
+* Tornar o número positivo, deverá multiplicar por -1
+ex: 
+numero = numero * -1;
+
+* Uma forma de incremento:
+numero++;
+        
+int numero = 5;
+System.out.println( ++ numero);
+System.out.println(numero);
+
+ */
