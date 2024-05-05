b6326d50e73adce0fa70a7cc27c20f1527a41848
sousa <mauriciocareca78@gmail.com>
2024-05-05 14:13:29 -0300

    update

---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,26 @@
+
+import java.security.PublicKey;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Exemplo de comentário in line
+
+        /*
+
+        Exemplo de comentário multi line
+
+        */
+
+        /** 
+        * Estas duas estrelinhas acima
+        * é para identificar que você
+        * pretende elaborar um comentário
+        * a nível de documentação.
+        * Que incrível !!!
+        */
+
+        Public void metodo(){
+
+        }
+    }
+}
