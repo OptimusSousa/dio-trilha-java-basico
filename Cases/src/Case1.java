7fb81f953cefe17cd43110bbb8f84233d261f396
sousa <mauriciocareca78@gmail.com>
2024-05-11 14:48:49 -0300

    update

---
+++ Cases/src/Case1.java
@@ -0,0 +1,18 @@
+public class Case1 {
+    public static void main(String[] args) throws Exception {
+        
+        double salarioBase = 2000.00;
+        double salarioCandidato = 2500.00;
+
+        if (salarioBase > salarioCandidato) {
+            System.out.println("Ligar para o candidato");
+        }
+        else if (salarioBase == salarioCandidato) {
+                System.out.println("Ligar para o candidato, com contra proposta");
+        }
+        else {
+                System.out.println("Aguardando resultado dos demais candidatos");
+        }
+        
+    }
+}
